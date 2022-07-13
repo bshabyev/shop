885d8d47e308789e93dd6b1be2320feaf6aeae65
bshabyev <bshabyev@gmail.com>
2022-07-13 21:24:19 +0600

    shop4ki

--- shop/settings.py
+++ shop/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-    # 'django-insecure-djp*s^g7--li#)pdv8*_-400=0o-=_*(-4#2!o=xtwwea+9%r!'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
